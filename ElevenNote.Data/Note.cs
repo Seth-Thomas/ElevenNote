536e2b5cb5ce6f8993dc8f6c462174e4e31ad2d9
Seth-Thomas <Seth.b.thomas8@gmail.com>
2021-01-29 16:20:29 -0500

    Update

---
+++ ElevenNote.Data/Note.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Data
+{
+    public class Note
+    {
+        [Key]
+        public int NoteId { get; set; }
+        [Required]
+        //[Range(1,10, ErrorMessage="please choose a number between 1 and 10")]
+        //[MaxLength(100, ErrorMessage="too many characters in this field")]
+        //[Display(Name="Your Name")]
+        public Guid OwnerId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [Required]
+        
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
